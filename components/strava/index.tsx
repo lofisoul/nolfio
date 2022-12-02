dcb3e763b70fa29125a9c6677525c53cb6abcfdf
Jason Nolfi <jason.nolfi@gmail.com>
2022-12-02 12:42:28 -0500

    feat: Integrate Strava API

--- components/strava/index.tsx
+++ components/strava/index.tsx
@@ -1,26 +1,23 @@
 import { useState, useEffect } from "react";
-import { strava, callRefresh } from "./utils";
 
 export function Strava() {
   const [activities, setActivities] = useState({});
 
   // Use refresh token to get current access token
   useEffect(() => {
-    fetch(callRefresh, {
-      method: "POST",
-    })
+    fetch("/api/strava")
       .then((res) => res.json())
-      .then((result) => getActivities(result.access_token));
-  }, [callRefresh]);
+      .then((data) => setActivities(data.response));
+  }, []);
 
   // use current access token to call all activities
-  function getActivities(access) {
-    // console.log(callActivities + access)
-    fetch(strava.callActivities + access)
-      .then((res) => res.json())
-      .then((data) => setActivities(data))
-      .catch((e) => console.log(e));
-  }
+  // function getActivities(access) {
+  //   // console.log(callActivities + access)
+  //   fetch(strava.callActivities + access)
+  //     .then((res) => res.json())
+  //     .then((data) => setActivities(data))
+  //     .catch((e) => console.log(e));
+  // }
 
   function showActivities() {
     console.log(activities);
